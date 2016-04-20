eae396415501ee9a7491c3e3d4d204f0bc7db87e
AndreyZelenskiy <zvukach70@gmail.com>
2016-04-20 22:21:49 +0300

    fix bug

--- app/src/main/java/com/example/talizorah/finalapp/cashMachine/AbstractLoader.java
+++ app/src/main/java/com/example/talizorah/finalapp/cashMachine/AbstractLoader.java
@@ -8,7 +8,7 @@ import java.util.List;
  * Created by xoll on 20.04.16.
  */
 public abstract class AbstractLoader {
-    protected String url = "https://api.privatbank.ua/p24api/infrastructure?json&atm&address=&city=%D0%9A%D0%BE%D0%BD%D1%81%D1%82%D0%B0%D0%BD%D1%82%D0%B8%D0%BD%D0%BE%D0%B2%D0%BA%D0%B0";
+    protected String url = "https://api.privatbank.ua/p24api/infrastructure?json&atm&address=%D0%BF%D0%BE%D0%B1%D0%B5%D0%B4%D1%8B&city=%D0%9A%D0%B8%D0%B5%D0%B2";
     protected String prefsName = "cashMachineJson";
 
     public abstract String getJson(Activity context);
